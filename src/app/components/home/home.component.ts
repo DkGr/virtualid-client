7c490ccd0d8b8803fd27a37c3110e2ac0efacf9e
Nourredine OCTEAU <nourredine.octeau@miodex.com>
2019-05-15 17:53:53 +0200

    Add login form

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -16,9 +16,15 @@ export class HomeComponent implements OnInit {
 
   registerForm: FormGroup;
 
-  user: User = new User();
+  loginForm: FormGroup;
+
+  loginUser: User = new User();
+
+  registerUser: User = new User();
 
   @ViewChild('generateKeysDialog') generateKeysDialog;
+
+  @ViewChild('loggingInDialog') loggingInDialog;
 
   constructor(private userService: UserService) { }
 
@@ -27,6 +33,10 @@ export class HomeComponent implements OnInit {
       registerUsername: new FormControl(null, [Validators.required, Validators.minLength(5), Validators.maxLength(15), Validators.pattern('[a-z0-9]*')]),
       registerEmail: new FormControl(null, [Validators.required, Validators.email]),
       registerPassword: new FormControl(null, [Validators.required, Validators.minLength(8)])
+    });
+    this.loginForm = new FormGroup({
+      loginUsername: new FormControl(null, Validators.required),
+      loginPassword: new FormControl(null, Validators.required)
     });
   }
 
@@ -42,22 +52,51 @@ export class HomeComponent implements OnInit {
     return this.registerForm.get('registerPassword');
   }
 
+  get loginUsernameInput() {
+    return this.loginForm.get('loginUsername');
+  }
+
+  get loginPasswordInput() {
+    return this.loginForm.get('loginPassword');
+  }
+
   async onRegister() {
     this.generateKeysDialog.show();
     var options = {
         userIds: [{
-          name: this.user.username,
+          name: this.registerUser.username,
           email: "${this.user.username}@virtualid.fr"
         }],
         numBits: 4096,
-        passphrase: this.user.password
+        passphrase: this.registerUser.password
     };
 
     var keys = await openpgp.generateKey(options);
-    this.user.publicKey = keys.publicKeyArmored;
-    this.user.privateKey = keys.privateKeyArmored;
+    this.registerUser.publicKey = keys.publicKeyArmored;
+    this.registerUser.privateKey = keys.privateKeyArmored;
 
-    this.userService.registerUser(this.user)
+    this.userService.registerUser(this.registerUser)
+      .subscribe(
+        data => this.onRegisterSuccess(),
+        error => this.onRegisterFailed(error)
+      );
+  }
+
+  onRegisterSuccess() {
+    this.generateKeysDialog.hide();
+    this.registerUser = new User();
+    this.registerForm.reset();
+  }
+
+  onRegisterFailed(error) {
+    console.log(error);
+    this.generateKeysDialog.hide();
+  }
+
+  async onLogin() {
+    this.loggingInDialog.show();
+
+    this.userService.loginUser(this.loginUser)
       .subscribe(
         data => this.onRegisterSuccess(),
         error => this.onRegisterFailed(error)
